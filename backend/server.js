32bde08b2e482322e4d779815ad0830f897a22fe
hritik <hc@rapidinnovation>
2024-01-11 00:48:08 +0530

    proxy added

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ async function main() {
     useNewUrlParser: true,
   });
   const app = express();
-  app.use(cors());
+  // app.use(cors());
   app.use(express.json());
   app.use("/api", routes);
 
